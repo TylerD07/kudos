e6f5db3afa94be04a14e8c3df82acd208f8007c9
TylerD07 <tyler.daughtry@aol.com>
2018-11-26 22:30:55 -0500

    added file structure, wrote server file and finsihed js file, still need to do alot more tommorw

---
+++ routes/api-routes.js
@@ -0,0 +1,66 @@
+const User = require("../models/User");
+const Kudos = require("../models/Kudos");
+
+module.exports = function(app) {
+  app.get("/api/users", function(req, res) {
+    User.find({})
+      .populate("kudos")
+      .then(function(data) {
+        console.log("data", data);
+        res.json(data);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
+
+  
+  app.post("/api/user", function(req, res) {
+    User.create(req.body)
+      .then(function(data) {
+        res.json(data);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
+
+
+  app.get("/api/kudos", function(req, res) {
+    Kudos.find({})
+      .then(function(dbKudos) {
+        res.json(dbKudos);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
+
+
+  app.post("/api/kudos", function(req, res) {
+    const newEntry = {
+      title: req.body.title,
+      body: req.body.body,
+      to: req.body.to,
+      from: req.body.from
+    };
+
+    Kudos.create(newEntry)
+      .then(function(kudosData) {
+        return User.updateMany(
+          { _id: req.body.to_id } || { _id: req.body.from_id },
+          {
+            $push: {
+              kudos: kudosData._id
+            }
+          }
+        );
+      })
+      .then(function(userData) {
+        res.json(userData);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
+};
